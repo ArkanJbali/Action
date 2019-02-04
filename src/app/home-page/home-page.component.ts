5b233b839875315389bcba0b8cee5ac01196002c
R@mez <ramez.abu.ahmd@gmail.com>
2019-02-04 18:12:01 +0200

    sdafghjk

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,8 +7,9 @@ import { MatSort, MatPaginator, MatTableDataSource } from '@angular/material';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-// this is ramiz
+// this is ramizzzzzz
 
+// 84512548452548956
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatPaginator) paginator2: MatPaginator;
